4618dbe576cb8ee910226b01bcb644528eaed01b
Mauricio Lima <mauricio@collateralxp.com>
2024-04-04 21:18:00 -0300

    add a way to have wrapline

--- nvim/lua/mauricio/core/keymaps.lua
+++ nvim/lua/mauricio/core/keymaps.lua
@@ -6,6 +6,8 @@ local keymap = vim.keymap
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("n", "<leader>L", ":Lazy<CR>", { desc = "Open Lazy panel" })
 keymap.set("n", "<leader>t", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal" })
+keymap.set("n", "<leader>cc", "<cmd>set colorcolumn=100<CR>", { desc = "Enable wrapline" })
+keymap.set("n", "<leader>cx", "<cmd>set colorcolumn=0<CR>", { desc = "Disable wrapline" })
 
 -- copilot
 vim.keymap.set("i", "<C-Y>", 'copilot#Accept("\\<CR>")', {
--- nvim/lua/mauricio/core/options.lua
+++ nvim/lua/mauricio/core/options.lua
@@ -60,3 +60,4 @@ opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 opt.scrolloff = 5
 opt.wrap = false
+opt.colorcolumn = "100"
