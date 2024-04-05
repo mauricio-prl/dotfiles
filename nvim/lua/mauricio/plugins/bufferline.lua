02f2e7097583137da6d5c6da3e9e45d4ec43dd9c
Mauricio Lima <mauricio@collateralxp.com>
2024-04-05 10:12:22 -0300

    finally fix bufferline config

--- nvim/lua/mauricio/plugins/bufferline.lua
+++ nvim/lua/mauricio/plugins/bufferline.lua
@@ -13,20 +13,10 @@ return {
     { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
     { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
   },
+  dependencies = "nvim-tree/nvim-web-devicons",
   opts = {
     options = {
-      -- stylua: ignore
-      close_command = function(n) require("mini.bufremove").delete(n, false) end,
-      -- stylua: ignore
-      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
-      diagnostics = "nvim_lsp",
       always_show_bufferline = false,
-      diagnostics_indicator = function(_, _, diag)
-        local icons = require("lazyvim.config").icons.diagnostics
-        local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-          .. (diag.warning and icons.Warn .. diag.warning or "")
-        return vim.trim(ret)
-      end,
       offsets = {
         {
           filetype = "neo-tree",
@@ -37,15 +27,4 @@ return {
       },
     },
   },
-  config = function(_, opts)
-    require("bufferline").setup(opts)
-    -- Fix bufferline when restoring a session
-    -- vim.api.nvim_create_autocmd("BufAdd", {
-    --   callback = function()
-    --     vim.schedule(function()
-    --       pcall(nvim_bufferline)
-    --     end)
-    --   end,
-    -- })
-  end,
 }
