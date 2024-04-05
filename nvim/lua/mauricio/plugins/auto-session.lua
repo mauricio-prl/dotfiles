2485906474be22bb4628791c44a6f9bd1ec37075
Mauricio Lima <mauricio@collateralxp.com>
2024-04-04 23:11:37 -0300

    lint

--- nvim/lua/mauricio/plugins/auto-session.lua
+++ nvim/lua/mauricio/plugins/auto-session.lua
@@ -10,6 +10,11 @@ return {
     })
 
     keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })
-    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" })
+    keymap.set(
+      "n",
+      "<leader>ws",
+      "<cmd>SessionSave<CR>",
+      { desc = "Save session for auto session root dir" }
+    )
   end,
 }
