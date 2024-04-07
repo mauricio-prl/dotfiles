71fdaaeacd7bcaedba95e3f1c940f4a52a4d73d3
Mauricio Lima <mauricio@collateralxp.com>
2024-04-07 00:57:19 -0300

    adjust nvim-tree settings
    remove them from autocommands

--- nvim/lua/mauricio/plugins/nvim-tree.lua
+++ nvim/lua/mauricio/plugins/nvim-tree.lua
@@ -52,9 +52,23 @@ return {
     -- set keymaps
     local keymap = vim.keymap
 
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>")
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>")
+    keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
+    keymap.set("n", "<leader>fe", "<cmd>NvimTreeFindFileToggle<CR>")
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>")
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>")
+
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 }
+      keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+      keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+      keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+      keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+      keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+      keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+      keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+    end
+
+    -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+    vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
   end,
 }
