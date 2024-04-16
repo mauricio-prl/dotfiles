b7720563046aa655073ef44b7292e47d6b0003d3
Mauricio Lima <mauricio@collateralxp.com>
2024-04-16 12:27:21 -0300

    Add vim-test with vimux
    remove toggleTerm

---
+++ nvim/lua/mauricio/plugins/vim-test.lua
@@ -0,0 +1,13 @@
+return {
+  "vim-test/vim-test",
+  dependencies = {
+    "preservim/vimux",
+  },
+  vim.cmd("let test#strategy = 'vimux'"),
+  -- keymaps
+  vim.keymap.set("n", "<leader>t", "<cmd>TestNearest<CR>"),
+  vim.keymap.set("n", "<leader>T", "<cmd>TestFile<CR>"),
+  vim.keymap.set("n", "<leader>a", "<cmd>TestSuite<CR>"),
+  vim.keymap.set("n", "<leader>l", "<cmd>TestLast<CR>"),
+  vim.keymap.set("n", "<leader>g", "<cmd>TestVisit<CR>"),
+}
