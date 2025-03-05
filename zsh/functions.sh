cb7c23435c42221ee2fd25986b919339ffe10b6d
Mauricio Lima <mauricio@collateralxp.com>
2025-03-05 19:46:28 -0300

    improve functions file
    add .sh extension to enable syntax highlight

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -1,7 +1,4 @@
-function kport() {
-  kill -9 $(lsof -t -i:$1)
-}
-
+### p10k
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
@@ -14,3 +11,32 @@ if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
         print -P "%F{33} %F{34}Installation successful.%f%b" || \
         print -P "%F{160} The clone has failed.%f%b"
 fi
+
+### kport
+function kport() {
+  kill -9 $(lsof -t -i:$1)
+}
+
+### repeat_cmd
+repeat_cmd() {
+  local times=$1
+  shift
+
+  # Input validation
+  if ! [[ "$times" =~ ^[0-9]+$ ]]; then
+    echo "Error: First argument must be a positive number" >&2
+    return 1
+  fi
+
+  if [ $# -eq 0 ]; then
+    echo "Error: No command specified to repeat" >&2
+    return 1
+  fi
+
+  # Use a counter variable with a more descriptive name
+  local iteration
+  for ((iteration = 1; iteration <= times; iteration++)); do
+    # Use eval to support shell aliases
+    eval "$@"
+  done
+}
