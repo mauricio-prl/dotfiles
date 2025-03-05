a93e2d325bab9b35164d3b8114e65b55df3a7c77
Mauricio Lima <mauricio@collateralxp.com>
2025-03-05 12:10:24 -0300

    use catppuccin color scheme on nvim

--- nvim/lua/mauricio/plugins/colorscheme.lua
+++ nvim/lua/mauricio/plugins/colorscheme.lua
@@ -1,7 +1,31 @@
+-- dracula
+-- return {
+--   "Mofiqul/dracula.nvim",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd([[colorscheme dracula]])
+--   end,
+-- }
+
+-- catppuccin
 return {
-  "Mofiqul/dracula.nvim",
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
   config = function()
-    vim.cmd([[colorscheme dracula]])
+    require("catppuccin").setup({
+      flavour = "macchiato", -- Change this to "latte", "frappe", "macchiato", or "mocha"
+      integrations = {
+        treesitter = true,
+        native_lsp = { enabled = true },
+        lsp_trouble = true,
+        cmp = true,
+        gitsigns = true,
+        telescope = true,
+        nvimtree = true,
+        which_key = true,
+      },
+    })
+    vim.cmd.colorscheme("catppuccin")
   end,
 }
